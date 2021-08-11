2e1473813793205545bada1e072ebfdb711873f8
Jordan Terry <jordan.terry@theguardian.com>
2021-08-11 12:59:26 +0100

    Remove 1.6 idea and another function

--- eraser/src/main/kotlin/uk/co/jordanterry/eraser/eraser.kt
+++ eraser/src/main/kotlin/uk/co/jordanterry/eraser/eraser.kt
@@ -10,16 +10,8 @@ fun LifecycleOwner.erase(animator: Animator) {
     lifecycle.addObserver(EraserLifecycleObserver(animator))
 }
 
-fun Animator.autoErase(lifecycleOwner: LifecycleOwner) {
-    lifecycleOwner.lifecycle.addObserver(EraserLifecycleObserver(this))
-}
-
-//Context(LifecycleOwner)
-//fun Animator.autoErase() {
-//    lifecycle.addObserver(EraserLifecycleObserver(this))
-//}
-
-private inline class EraserLifecycleObserver(private val animator: Animator) : LifecycleObserver {
+@JvmInline
+private value class EraserLifecycleObserver(private val animator: Animator) : LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun cancelAnimator() {
         animator.cancel()

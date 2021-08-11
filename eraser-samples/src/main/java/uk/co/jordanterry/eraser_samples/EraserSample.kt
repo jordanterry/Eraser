54eadb326641890446751c3305d4521df4a148d1
Jordan Terry <jordan.terry@theguardian.com>
2021-08-11 21:27:18 +0100

    Add sample project

---
+++ eraser-samples/src/main/java/uk/co/jordanterry/eraser_samples/EraserSample.kt
@@ -0,0 +1,11 @@
+package uk.co.jordanterry.eraser_samples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EraserSample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eraser_sample)
+    }
+}
--- eraser/src/main/kotlin/uk/co/jordanterry/eraser/eraser.kt
+++ eraser/src/main/kotlin/uk/co/jordanterry/eraser/eraser.kt
@@ -1,19 +1,61 @@
 package uk.co.jordanterry.eraser
 
 import android.animation.Animator
+import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 
-fun LifecycleOwner.erase(animator: Animator) {
+/**
+ * Handle the lifecycle functions of an [Animator] as per the lifecycle of an Activity.
+ *
+ * * When the Activity is paused, [Animator.pause] will be called
+ * * When the Activity is resumed, [Animator.resume] will be called
+ * * When the Activity is destroyed, [Animator.cancel] will be called all listeners will be remove from the animator
+ *
+ * @param animator that must be erased
+ */
+fun ComponentActivity.erase(animator: Animator) {
     lifecycle.addObserver(EraserLifecycleObserver(animator))
 }
 
+/**
+ * Handle the lifecycle functions of an [Animator] as per the lifecycle of a Fragment's View.
+ *
+ * * When the Fragment is paused, [Animator.pause] will be called
+ * * When the Fragment is resumed, [Animator.resume] will be called
+ * * When the Fragment's View is destroyed, [Animator.cancel] will be called all listeners will be remove from the animator
+ *
+ * @param animator that must be erased
+ */
+fun Fragment.erase(animator: Animator) {
+    viewLifecycleOwner.lifecycle.addObserver(EraserLifecycleObserver(animator))
+}
+
+/**
+ * Observes lifecycle events and calls lifecycle methods on a provided [Animator].
+ *
+ * @property animator to be changes as the lifecycle being observed moves through different states
+ */
 @JvmInline
-private value class EraserLifecycleObserver(private val animator: Animator) : LifecycleObserver {
+private value class EraserLifecycleObserver(
+    private val animator: Animator
+) : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun resumeAnimator() {
+        animator.resume()
+    }
+
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun pauseAnimator() {
+        animator.pause()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun cancelAnimator() {
         animator.cancel()
+        animator.removeAllListeners()
     }
 }
